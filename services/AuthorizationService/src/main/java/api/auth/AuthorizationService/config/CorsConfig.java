0780cfa48f22d5d2548379458247203a6bee9358
KJH0476 <148829100+KJH0476@users.noreply.github.com>
2024-10-13 14:47:06 +0900

    Update: cors 설정 추가

---
+++ services/AuthorizationService/src/main/java/api/auth/AuthorizationService/config/CorsConfig.java
@@ -0,0 +1,29 @@
+package api.auth.AuthorizationService.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfig {
+
+    @Value("${cors.origin}")
+    private String origin;
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin(origin);
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+}
